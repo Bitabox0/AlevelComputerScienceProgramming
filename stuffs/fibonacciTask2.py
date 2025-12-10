16d04230af6aad980f39bfeb6f15a3db6961bd53
ivan Williams <20WilliamsI@cfbs.islington.sch.uk>
2025-12-10 10:08:15 +0000

    recursive vs iterative fibonacci funtions testing

--- stuffs/fibonacciTask2.py
+++ stuffs/fibonacciTask2.py
@@ -1,11 +1,31 @@
+import time as t
+
 def fib(n):
     if n <= 1:
         return n
     else:
         return fib(n-1) + fib(n-2)
 
-x = fib(7)
-print(x)
+
 
 def fibonacci2(n):
-    x=1
+    fibNumbers = [0, 1]
+
+    for i in range(2, n+1):
+        fibNumbers.append(fibNumbers[i-1] + fibNumbers[i-2])
+    
+    return fibNumbers[n]
+
+start1 = t.time()
+fib(30)
+stop1 = t.time()
+totalTime1 = stop1 - start1
+formatTime1 = "{:.10f}".format(totalTime1)
+print("recursive", formatTime1)
+
+start2 = t.time()
+fibonacci2(30)
+stop2 = t.time()
+totalTime2 = stop2 - start2
+formatTime2 = "{:.10f}".format(totalTime2)
+print("iterative", formatTime2)
